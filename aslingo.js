76ab0422405a5aad707123ba31bf64666a874f3b
Javier Rivera <Cosmo.Adrift@gmail.com>
2026-09-22 23:56:16 -0700

    Adding Javascript file with the some words and a source of the sign

---
+++ aslingo.js
@@ -0,0 +1,8 @@
+// aslingo.js
+
+// Simple ASL "library" – later you’ll load this from your backend/DB
+const aslSigns = [
+    { id: 1, word: "Hello", src: "https://www.youtube.com/embed/FVjpLa8GqeM?rel=0%3Bautoplay%3D1" },
+    { id: 2, word: "Thank you", src: "https://www.youtube.com/embed/IvRwNLNR4_w?rel=0%3Bautoplay%3D1" },
+    { id: 3, word: "Yes", src: "https://www.youtube.com/embed/0usayvOXzHo?rel=0%3Bautoplay%3D1" }
+];
